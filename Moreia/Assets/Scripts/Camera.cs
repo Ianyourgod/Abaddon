cf7bb8724d24f41e9a34aa42814ff44542705a45
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2024-07-27 18:31:36 -0500

    push to main EVILLLL // camera is fps independent

--- Moreia/Assets/Scripts/Camera.cs
+++ Moreia/Assets/Scripts/Camera.cs
@@ -7,21 +7,13 @@ public class Camera : MonoBehaviour
     [SerializeField] float lerpSpeed = 0.1f;
 
     void Update() {
-        if (!Controller.main) return;
+        if (Controller.main == null) return;
 
         Vector2 playerPosition = Controller.main.transform.position;
         Vector2 cameraPosition = transform.position;
 
-        Vector2 newPosition = Lerp(cameraPosition, playerPosition, lerpSpeed);
+        Vector2 newPosition = Vector2.Lerp(cameraPosition, playerPosition, lerpSpeed);
 
         transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
-    }
-
-    float Lerp(float a, float b, float t) {
-        return a + (b - a) * t;
-    }
-
-    Vector2 Lerp(Vector2 a, Vector2 b, float t) {
-        return new Vector2(Lerp(a.x, b.x, t), Lerp(a.y, b.y, t));
     }
 }

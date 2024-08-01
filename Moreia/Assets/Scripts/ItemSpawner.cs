76198da165381c368d86b38f5d4a341980e577ef
Saturn-VI <112597058+Saturn-VI@users.noreply.github.com>
2024-08-01 10:08:58 -0500

    move prefab folder

--- Moreia/Assets/Scripts/ItemSpawner.cs
+++ Moreia/Assets/Scripts/ItemSpawner.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class ItemSpawner : MonoBehaviour
 {
+    public enum TableTypes
+    {
+        Goblin,
+        Pixie,
+        Barrel,
+        Pot
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,21 @@ public class ItemSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public int SpawnRandom(TableTypes table)
+    {
+        switch (table)
+        {
+            case TableTypes.Goblin:
+                print("yippee");
+                Instantiate((UnityEngine.Object) Resources.Load($"Prefabs/Equipment/{Controller.main.rnd.Next(2, 6)}"), transform.position, Quaternion.identity);
+                break;
+            default:
+                break;
+        }
+        Destroy(gameObject);
+        return 1;
     }
 }

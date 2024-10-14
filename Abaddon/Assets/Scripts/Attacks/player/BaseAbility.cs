dcaed3d7174fbb363b80379eed82efe302b633b6
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2024-10-14 12:11:42 -0500

    add enabled bool

--- Abaddon/Assets/Scripts/Attacks/player/BaseAbility.cs
+++ Abaddon/Assets/Scripts/Attacks/player/BaseAbility.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseAbility : MonoBehaviour {
+    public bool enbaled = false;
+
     public virtual bool CanUse(Collider2D collider, Vector2 direction) {
+        if (!enbaled) return false;
+
         if (collider == null) return false;
         
         return collider.gameObject.layer == LayerMask.NameToLayer("Enemy");

8d6df8669ebb7739db60141cbdae1e55e1aa8df0
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2025-01-06 17:35:03 -0600

    wow attack the boss but without damage. crazy.

--- Abaddon/Assets/Scripts/Attacks/player/NormalAttack.cs
+++ Abaddon/Assets/Scripts/Attacks/player/NormalAttack.cs
@@ -6,6 +6,15 @@ using UnityEngine;
 public class NormalAttack : BaseAbility {
     public override void Attack(Collider2D hit, Vector2 direction, Animator animator, PlayerSfx sfxPlayer) {
         // rahh im attacking!!
+
+        // check if the hit object has "BossScript" component
+        if (hit.gameObject.GetComponent<Boss1>() != null) {
+            animator.GetComponent<Renderer>().sortingLayerID = SortingLayer.NameToID("AttackerLayer");
+            sfxPlayer.PlaySwoosh();
+            Controller.main.PlayAnimation("attack", direction);
+            return;
+        }
+
         hit.gameObject.GetComponent<EnemyMovement>().DamageEnemy(Convert.ToUInt32(Controller.main.attackDamage), hit.gameObject.tag);
         animator.GetComponent<Renderer>().sortingLayerID = SortingLayer.NameToID("AttackerLayer");
         sfxPlayer.PlayAttackSound();
--- Abaddon/Assets/Scripts/Audio/CharacterSfx.cs
+++ Abaddon/Assets/Scripts/Audio/CharacterSfx.cs
@@ -21,11 +21,19 @@ public class CharacterSfx : SfxPlayer
     [SerializeField] AudioClip[] attackSfx;
     [Tooltip("Whether the action will play a random sound effect from the list or not")]
     [SerializeField] bool randomAttackSound = false;
+    [Space]
+
+    [Tooltip("Include all swoosh sound effects for this character")]
+    [SerializeField] AudioClip[] swooshSfx;
+    [Tooltip("Whether the action will play a random sound effect from the list or not")]
+    [SerializeField] bool randomSwooshSound = false;
+    [Space]
 
     [Tooltip("Include all death sound effects for this character")]
     [SerializeField] AudioClip[] deathSfx;
     [Tooltip("Whether the action will play a random sound effect from the list or not")]
     [SerializeField] bool randomDeathSound = false;
+    [Space]
 
     [Header("CharacterSfx Attributes")]
     [Tooltip("(volume is in 0.01 scale) How much volume you want to add to your walk sound effects")]
@@ -73,5 +81,14 @@ public class CharacterSfx : SfxPlayer
         }
 
         PlaySfx(deathSfx[0], addedDeathVolume);
+    }
+
+    public void PlaySwoosh() {
+        if (randomSwooshSound){
+            PlayRandomSound(swooshSfx, 0);
+            return;
+        }
+
+        PlaySfx(swooshSfx[0], 0);
     }
 }

b0841dea00e0e2a31c0c0978a3bd2f36bf3d588f
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2025-01-06 21:18:42 -0600

    wow spawn the statue

--- Abaddon/Assets/Scripts/Characters/Boss1.cs
+++ Abaddon/Assets/Scripts/Characters/Boss1.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Boss1 : MonoBehaviour
 {
+    [SerializeField] GameObject statuePrefab;
+
     [HideInInspector] public bool inFight = false;
     [HideInInspector] public int stage = 0;
 
@@ -11,5 +13,21 @@ public class Boss1 : MonoBehaviour
         inFight = true;
         stage = 1;
         Debug.Log("i am 1ssoB, and i hate");
+        SpawnStatue();
+    }
+
+    private void SpawnStatue() {
+        Debug.Log("i am 1ssoB, and i spawn statue");
+
+        // generate statue position
+        Vector2 bossPosition = transform.position;
+        // man i hate unity
+        Vector2 random_position = Random.insideUnitCircle.normalized * 3;
+        // normalize to (integer + .5)
+        Vector2 statuePosition = new Vector2(Mathf.Round(random_position.x), Mathf.Round(random_position.y)) + bossPosition;
+
+        GameObject statue = Instantiate(statuePrefab, statuePosition, Quaternion.identity);
+
+        statue.GetComponent<Statue>().boss = this;
     }
 }
---
+++ Abaddon/Assets/Scripts/Characters/Statue.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Statue : MonoBehaviour
+{
+    [HideInInspector] public Boss1 boss;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

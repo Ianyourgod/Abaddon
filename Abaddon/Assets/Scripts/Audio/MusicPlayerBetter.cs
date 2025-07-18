8df774515c59882a5d3e369eae6f566aa9d7469d
Saturn-VI <maxwang420@gmail.com>
2025-07-18 10:41:24 -0500

    Make the main menu theme quieter + add win theme

--- Abaddon/Assets/Scripts/Audio/MusicPlayerBetter.cs
+++ Abaddon/Assets/Scripts/Audio/MusicPlayerBetter.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class MusicPlayerBetter : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]
+    private string songName = "level1";
+
     void Start()
     {
-        AudioManagerBetter.main.PlaySong("level1");
+        AudioManagerBetter.main.SetLooping(true);
+        AudioManagerBetter.main.PlaySong(songName);
     }
 
     // Update is called once per frame
